31ed18990f1f4d349db087a913207855a8f73175
Çağdaş Yarıcı <cagdasyarici16@gmail.com>
2023-04-10 02:46:38 +0300

    EntityUpdate

---
+++ IdentityPractice/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata;
+
+namespace IdentityPractice.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+        public Category()
+        {
+            Posts = new List<Post>();
+        }
+    }
+}
