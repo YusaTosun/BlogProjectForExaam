a40fb2e8745f876f2ca47b328a1314fc351806d9
Yusa Tosun <yusatosun.yt@gmail.com>
2023-04-10 12:30:45 +0300

    12:30

---
+++ IdentityPractice/Models/ViewModels/PostVM.cs
@@ -0,0 +1,15 @@
+﻿using IdentityPractice.Entities;
+
+namespace IdentityPractice.Models.ViewModels
+{
+    public class PostVM
+    {
+        public Post Post { get; set; }
+        public ICollection<Post> Posts { get; set; }
+
+        public PostVM()
+        {
+            Posts = new List<Post>();
+        }
+    }
+}
